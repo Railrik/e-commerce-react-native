941faaaeb7eef85fb9a01d5bf22155f56adf2607
Railrik <benjamin@ligny.pro>
2023-10-13 22:26:35 +0200

    ajout de l'historique d'achats

--- screens/PaymentsHistory.js
+++ screens/PaymentsHistory.js
@@ -1,11 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { useSelector } from 'react-redux'
+import EmptyItem from '../components/EmptyItem';
+import PaidItems from '../components/PaidItems';
 
 const PaymentsHistory = () => {
-    return (
-        <View>
-            <Text>PaymentsHistory</Text>
-        </View>
-    )
+
+    const payments = useSelector(state => state.payments.payments);
+
+    const parsedPayments = payments.map(state => JSON.parse(state));
+    if (payments.length > 0) {
+        return (
+            <FlatList
+                data={parsedPayments}
+                renderItem={({ item }) => (
+                    <PaidItems
+                        totalPrice={item.total}
+                        date={item.date}
+                        courseDetails={item}
+                    ></PaidItems>
+                )}
+            />
+        )
+    }
+    return <EmptyItem text="Pas d'achats'" />
 }
 
 export default PaymentsHistory
